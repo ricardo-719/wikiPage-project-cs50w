09a5e3413af8dcc1cfdba5cf55a9def69706fc94
ricardo-719 <teacherrickyricardo@gmail.com>
2022-12-21 18:48:33 +0700

    Enable search correct matches redirection

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("create", views.create_entry, name="create"),
+    path("search", views.search_page, name="searchPage"),
     path("<str:name>", views.read_page, name="readPage")
     
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -26,18 +26,18 @@ def read_page(request, name):
         return render(request, "encyclopedia/entry-page.html", {
             "name": name.capitalize(),
             "entry": markdown.markdown(entry)
-            })
+        })
 
 # Allow user to input a new entry
 # Create form using the Django method / 1hr 19mins into the lecture...
 def create_entry(request):
     if request.method == "POST":
         form = newPageEntry(request.POST)
-        #Figure out how to add both Title and Content!!
+        #Figure out how to add both Title and Content!! CONTINUE VID ON SESSIONS TIMEMARK
         if form.is_valid():
             title = form.cleaned_data["Title"]
             content = form.cleaned_data["Content"]
-            entries.append(content)
+            # Use the util function to save
             return HttpResponseRedirect(reverse("index"))
         else:
             return render(request, "encyclopedia/create-entry.html", {
@@ -46,3 +46,23 @@ def create_entry(request):
     return render(request, "encyclopedia/create-entry.html", {
         "form": newPageEntry()
     })
+
+def search_page(request):
+    if request.method == "POST":
+        searchQuery = request.POST.get('q')
+        fetchEntry = util.get_entry(searchQuery)
+        ######## CONTINUE HERE
+        if fetchEntry == None:
+            entries = util.list_entries()
+            queryMatches = str for str in entries if any(sub in str for sub in searchQuery) #entries and then have some sort of a function that sort it out
+            #Here the page must display all the search elements that partially match the query
+            return render(request, "encyclopedia/search-page.html", {
+                "entries": queryMatches
+                })
+        else:
+            return HttpResponseRedirect(f"/{searchQuery}")
+            
+            #render(request, "encyclopedia/entry-page.html", {
+             #   "entry": markdown.markdown(fetchEntry)
+            #})
+        ######## CONTINUE HERE
